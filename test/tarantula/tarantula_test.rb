de17ab8c54a894a4c7bd0a27230eec16cd39f18f
Andreas Maierhofer <a.maierhofer@gmail.com>
2018-06-20 11:12:10 +0200

    Do not let tarantula crawl as non existing user

--- test/tarantula/tarantula_test.rb
+++ test/tarantula/tarantula_test.rb
@@ -18,28 +18,15 @@ class TarantulaTest < ActionDispatch::IntegrationTest
   reset_fixture_path File.expand_path('../../../spec/fixtures', __FILE__)
 
   include TarantulaConfig
-  
+
   # Crawls the application with admin permissions
   # to cover as many actions as possible.
   def test_tarantula_as_admin
     crawl_as(people(:admin))
   end
 
-  # Crawls the application in the most common administrative role
-  # on the base layer as an intermediate user.
-  def test_tarantula_as_leader
-    crawl_as(people(:leader))
-  end
-
-  # Crawls the application with almost no permissions in the
-  # the most common member role to check the public pages and
-  # the absence of links to permission denied actions.
-  def test_tarantula_as_member
-    crawl_as(people(:member))
-  end
-  
   private
-  
+
   def configure_urls_with_hitobito_sbv(t, person)
     configure_urls_without_hitobito_sbv(t, person)
 
