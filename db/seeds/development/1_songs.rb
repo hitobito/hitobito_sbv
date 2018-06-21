cd548ef64d5a97dd550b3ee6955461f9fa527e79
Andreas Maierhofer <a.maierhofer@gmail.com>
2018-06-21 16:25:22 +0200

    Insert / update  license headers

--- db/seeds/development/1_songs.rb
+++ db/seeds/development/1_songs.rb
@@ -1,3 +1,10 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2018, Schweizer Blasmusikverband. This file is part of
+#  hitobito_sbv and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sbv.
+
 def one_or_many
   3.times.collect { Faker::Book.author }.sample(rand(1..3)).join(' / ')
 end
