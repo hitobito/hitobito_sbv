0b950cedb35db8f1ef8aa68cb0d6deb452294a7a
Matthias Viehweger <viehweger@puzzle.ch>
2018-06-22 10:20:48 +0200

    Inject Regionalverbände in the hierarchy

--- db/seeds/development/0_groups.rb
+++ db/seeds/development/0_groups.rb
@@ -25,12 +25,18 @@ def limited(collection, selection: nil)
   end
 end
 
-def build_verein_attrs(parent_id, name)
+def build_verein_attrs(parent_id, name, besetzung, lang)
   { name: name, parent_id: parent_id,
     address: Faker::Address.street_address,
     zip_code: Faker::Address.zip[0..3],
-    town: Faker::Address.city
+    town: Faker::Address.city,
+    besetzung: besetzung,
+    correspondence_language: lang,
   }
+end
+
+def build_regionalverband_attrs(parent_id, name = nil)
+  build_verein_attrs(parent_id, (name || Faker::Address.city), nil, nil)
 end
 
 csv = CSV.parse(Wagons.find('sbv').root.join('db/seeds/development/vereine.csv').read, headers: true)
@@ -42,11 +48,21 @@ limited(csv.by_col['Verband'].uniq, selection: [
 ]).each do |name|
   mv = Group::Mitgliederverband.seed(:name, :parent_id, name: name, parent_id: root.id).first
 
-  verein_attrs = csv.select { |row| row['Verband'] == name }.collect do |row|
-    build_verein_attrs(mv.id, row['Verein'])
+  regionalverband_attrs = csv.select { |row| row['Verband'] == name }.collect do |row|
+    build_regionalverband_attrs(mv.id, row['Regionalverband'])
   end
 
-  Group::Verein.seed(:name, :parent_id, limited(verein_attrs))
+  rvs = Group::Regionalverband.seed(:name, :parent_id, limited(regionalverband_attrs))
+
+  rvs.each do |rv|
+    verein_attrs = csv.select do |row|
+      row['Verband'] == name && (row['Regionalverband'] == rv.name || row['Regionalverband'].nil?)
+    end.collect do |row|
+      build_verein_attrs(rv.id, row['Verein'], row['Besetzung'], row['Amtssprache'])
+    end
+
+    Group::Verein.seed(:name, :parent_id, limited(verein_attrs))
+  end
 
   mv.default_children.each do |child_class|
     child_class.first.update_attributes(seeder.group_attributes)
