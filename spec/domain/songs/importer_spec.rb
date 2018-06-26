355fe1883c4b0fe15ad25a70b5cc3d4c09013ad7
Janiss Binder <binder@puzzle.ch>
2018-06-26 17:32:04 +0200

    Test stdout with given matchers
    refs hitobito/hitobito#582

--- spec/domain/songs/importer_spec.rb
+++ spec/domain/songs/importer_spec.rb
@@ -11,19 +11,21 @@ describe Songs::Importer, type: :domain do
 
   it 'puts file not found if file does not exist' do
     expect(File).to receive(:exists?).and_return(false)
-    expect(STDOUT).to receive(:puts).with('File not found')
 
-    Songs::Importer.new('unknown.csv').compose
+    expect do
+      Songs::Importer.new('unknown.csv').compose
+    end.to output("File not found\n").to_stdout
   end
 
   it 'should import songs correctly' do
-    expect(STDOUT).to receive(:puts).exactly(3).times.and_return(nil)
     expect(File).to receive(:exists?).and_return(true)
     expect(CSV).to receive(:read).and_return(example_csv)
 
     expect do
-      Songs::Importer.new('example.csv').compose
-    end.to change{ Song.count }.by(3)
+      expect do
+        Songs::Importer.new('example.csv').compose
+      end.to change{ Song.count }.by(3)
+    end.to output(/Successfully inserted 3 songs/).to_stdout
 
     expect(Song.find_by(title: 'title1').composed_by).to eq('composer1')
     expect(Song.find_by(title: 'title1').arranged_by).to eq('arrangor1')
