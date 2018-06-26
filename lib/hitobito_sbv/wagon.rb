bac49370eaf9766316b9c9825674670f4f702892
Matthias Viehweger <viehweger@puzzle.ch>
2018-06-26 08:59:32 +0200

    Conform to linter and styleguide

--- lib/hitobito_sbv/wagon.rb
+++ lib/hitobito_sbv/wagon.rb
@@ -11,13 +11,13 @@ module HitobitoSbv
     app_requirement '>= 0'
 
     # Add a load path for this specific wagon
-    config.autoload_paths += %W( #{config.root}/app/abilities
-                                 #{config.root}/app/domain
-                                 #{config.root}/app/jobs
-                               )
+    config.autoload_paths += %W[
+      #{config.root}/app/abilities
+      #{config.root}/app/domain
+      #{config.root}/app/jobs
+    ]
 
     config.to_prepare do
-      # rubocop:disable SingleSpaceBeforeFirstArg
       # extend application classes here
       # models
       Group.send        :include, Sbv::Group
