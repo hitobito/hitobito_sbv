b5e7a68413f9f96b0cb7b14050e330712a5279bb
Matthias Viehweger <viehweger@puzzle.ch>
2018-07-02 17:03:22 +0200

    Add spec to verify basic controller behaviour
    See hitobito/hitobito#584

--- spec/controllers/song_censuses_controller_spec.rb
+++ spec/controllers/song_censuses_controller_spec.rb
@@ -6,17 +6,50 @@
 require 'spec_helper'
 
 describe SongCensusesController do
+  let(:group)   { Fabricate(Group::Regionalverband.name.to_sym) }
+  let(:verein1) { Fabricate(Group::Verein.name.to_sym, parent: group) }
+  let(:verein2) { Fabricate(Group::Verein.name.to_sym, parent: group) }
+
+  context 'index' do
+    let(:admin) { people(:admin) }
+
+    before do
+      Fabricate(Group::Regionalverband::SuisaAdmin.name.to_sym, group: group, person: admin)
+      sign_in(admin)
+    end
+
+    it 'shows which groups have submitted a census' do
+      get :index, group_id: group.id
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  context 'create' do
+    let(:admin) { people(:suisa_admin) }
+
+    before do
+      sign_in(admin)
+
+      SongCount.create(song: songs(:papa), verein: admin.primary_group, year: 2018, song_census: nil)
+      SongCount.create(song: songs(:girl), verein: admin.primary_group, year: 2018, song_census: nil)
+    end
+
+    it 'connects open song-counts to the current song-census' do
+      expect do
+        post :create, group_id: admin.primary_group
+      end.to change { SongCount.where(song_census: nil).count }.by(-2)
+    end
+  end
+
   context 'remind' do
-    let(:group)   { Fabricate(Group::Regionalverband.name.to_sym) }
-    let(:verein1) { Fabricate(Group::Verein.name.to_sym, parent: group) }
-    let(:verein2) { Fabricate(Group::Verein.name.to_sym, parent: group) }
     let(:song_census)  { song_censuses(:two_o_18) }
 
     before do
       sign_in(people(:suisa_admin))
 
       2.times { Fabricate(Group::Verein::SuisaAdmin.name.to_sym, group: verein1)}
-      Fabricate(Group::Verein::SuisaAdmin.name.to_sym, group: verein1)
+      Fabricate(Group::Verein::SuisaAdmin.name.to_sym, group: verein2)
     end
 
     it 'reminds suisa_admins' do
