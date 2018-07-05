c94348d07bf5b55ec1b62e7d31a52c9bcf7d4807
Matthias Viehweger <viehweger@puzzle.ch>
2018-07-05 16:08:55 +0200

    Spec CensusSubmission

---
+++ spec/domain/census_submission_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe CensusSubmission do
+  let(:census) { song_censuses(:two_o_18) }
+  let(:group) { groups(:musikgesellschaft_aarberg) }
+  subject { described_class.new(group, census) }
+
+  it 'submits song_counts by linking them to a census' do
+    song_counts(:son_count).update(song_census: nil)
+
+    expect do
+      expect( subject.submit ).to be_truthy
+    end.to change { SongCount.where(song_census: nil).count }.by(-1)
+  end
+
+  it 'does not change rows if everything is submitted already' do
+    expect do
+      expect( subject.submit ).to be_falsey
+    end.to_not change { SongCount.where(song_census: nil).count }
+  end
+end
