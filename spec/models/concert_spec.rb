d8f050125f37725b076f39fdfe34fad80e383ef9
Janiss Binder <binder@puzzle.ch>
2018-10-11 14:30:14 +0200

    Allow multiple concerts with same name

--- spec/models/concert_spec.rb
+++ spec/models/concert_spec.rb
@@ -34,13 +34,9 @@ describe Concert do
     end
 
     it 'sets name if nothing is given' do
-      concert1 = Concert.create!(verein: groups(:musikgesellschaft_alterswil), year: 2018)
-      concert2 = Concert.create!(verein: groups(:musikgesellschaft_alterswil), year: 2018)
-      concert3 = Concert.create!(verein: groups(:musikgesellschaft_alterswil), year: 2018)
+      concert = Concert.create!(verein: groups(:musikgesellschaft_alterswil), year: 2018)
 
-      expect(concert1.name).to eq 'Aufführung #1'
-      expect(concert2.name).to eq 'Aufführung #2'
-      expect(concert3.name).to eq 'Aufführung #3'
+      expect(concert.name).to eq 'Aufführung ohne Datum'
     end
 
     it 'removes empty song counts' do
@@ -54,22 +50,6 @@ describe Concert do
 
       expect(concert.song_counts.count).to be 1
       expect(concert.song_counts.first.song).to eq(songs(:girl))
-    end
-  end
-
-  context 'validations' do
-    it 'invalid name if already exists in verein and year' do
-      Concert.create(name: 'same_name', verein: groups(:musikgesellschaft_alterswil), year: 2018)
-      concert2 = Concert.create(name: 'same_name', verein: groups(:musikgesellschaft_alterswil), year: 2018)
-
-      expect(concert2.errors[:name]).to include 'ist bereits vergeben'
-    end
-
-    it 'valid name if not same year' do
-      Concert.create(name: 'same_name', verein: groups(:musikgesellschaft_alterswil), year: 2017)
-      concert2 = Concert.create(name: 'same_name', verein: groups(:musikgesellschaft_alterswil), year: 2018)
-
-      expect(concert2).to be_valid
     end
   end
 end
