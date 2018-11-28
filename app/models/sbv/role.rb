f5ba4509524bbbf6a9ed0b81a0c3bfa283c97253
Matthias Viehweger <viehweger@puzzle.ch>
2018-11-28 23:52:02 +0100

    Use validates_as_timeliness instead of own validations
    See hitobito/hitobito#602

--- app/models/sbv/role.rb
+++ app/models/sbv/role.rb
@@ -3,28 +3,22 @@ module Sbv::Role
 
   included do
     validates :created_at, presence: true, if: :deleted_at
-    validate :created_at_has_to_be_earlier_or_equal_than_today
-    validate :created_at_has_to_be_earlier_or_equal_than_deleted_at
+
+    validates_date :created_at,
+                   if: :deleted_at,
+                   on_or_before: :deleted_at,
+                   on_or_before_message: :cannot_be_later_than_deleted_at
+
+    validates_date :created_at,
+                   allow_nil: true,
+                   on_or_before: -> { Time.zone.today },
+                   on_or_before_message: :cannot_be_later_than_today
   end
 
   Role::Types::Permissions << :song_census
 
   class SuisaAdmin < ::Role
     self.permissions = [:group_read, :song_census]
-  end
-
-  private
-
-  def created_at_has_to_be_earlier_or_equal_than_deleted_at
-    if deleted_at && (!created_at || created_at > deleted_at)
-      errors.add(:created_at, I18n.t('errors.messages.cannot_be_later_than_deleted_at'))
-    end
-  end
-
-  def created_at_has_to_be_earlier_or_equal_than_today
-    if created_at && created_at.to_date > Time.zone.today
-      errors.add(:created_at, I18n.t('errors.messages.cannot_be_later_than_today'))
-    end
   end
 
 end
