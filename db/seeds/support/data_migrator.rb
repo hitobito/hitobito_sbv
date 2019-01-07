71dcd28bc2e92c7db39a8c5a380e9e47f1bc1247
Matthias Viehweger <viehweger@puzzle.ch>
2019-01-07 18:26:32 +0100

    Happy new year, you LICENSE-Headers

--- db/seeds/support/data_migrator.rb
+++ db/seeds/support/data_migrator.rb
@@ -1,3 +1,11 @@
+# encoding: utf-8
+
+#  Copyright (c) 2018 - 2019, Schweizer Blasmusikverband. This file is part of
+#  hitobito_sbv and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sbv.
+
+# Support for better readable migration/seed-files
 class DataMigrator
 
   def initialize(fn = nil)
