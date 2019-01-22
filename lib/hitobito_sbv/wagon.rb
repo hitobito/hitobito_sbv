44c1e77656430bc1a3ad733c08ffb075dd25e40d
Matthias Viehweger <viehweger@puzzle.ch>
2019-01-22 13:59:58 +0100

    Include patched children in ML-Subscriber lookup

--- lib/hitobito_sbv/wagon.rb
+++ lib/hitobito_sbv/wagon.rb
@@ -1,4 +1,4 @@
-#  Copyright (c) 2012-2018, Schweizer Blasmusikverband. This file is part of
+#  Copyright (c) 2012-2019, Schweizer Blasmusikverband. This file is part of
 #  hitobito_sbv and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_sbv.
@@ -23,6 +23,7 @@ module HitobitoSbv
       Group.send        :include, Sbv::Group
       Person.send       :include, Sbv::Person
       Role.send         :include, Sbv::Role
+      Subscription.send :prepend, Sbv::Subscription
 
       ### abilities
       RoleAbility.send :include, Sbv::RoleAbility
