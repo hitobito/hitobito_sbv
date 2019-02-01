b9ba1d0ab6f8bdc2be89ca337a97f7723e46db28
Matthias Viehweger <viehweger@puzzle.ch>
2019-02-02 00:39:13 +0100

    Update active years when adding historic roles
    See hitobito/hitobito#706

--- app/controllers/sbv/roles_controller.rb
+++ app/controllers/sbv/roles_controller.rb
@@ -1,3 +1,8 @@
+#  Copyright (c) 2012-2019, Schweizer Blasmusikverband. This file is part of
+#  hitobito_sbv and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sbv.
+
 module Sbv
   module RolesController
     extend ActiveSupport::Concern
@@ -6,7 +11,9 @@ module Sbv
       person_id = params[:role][:person_id]
       assign_history_attributes
 
-      unless entry.save
+      if entry.save
+        update_active_years_on_person(::Person.find(person_id))
+      else
         set_failure_notice
       end
 
@@ -15,6 +22,7 @@ module Sbv
 
     private
 
+    # rubocop:disable Style/RescueModifier
     def assign_history_attributes
       assign_attributes
       @group = @group.children.find_by(type: 'Group::VereinMitglieder')
@@ -22,6 +30,14 @@ module Sbv
       entry.type = 'Group::VereinMitglieder::Mitglied'
       entry.created_at = params[:role][:start_date].to_date rescue nil
       entry.deleted_at = params[:role][:end_date].to_date rescue nil
+    end
+    # rubocop:enable Style/RescueModifier
+
+    def update_active_years_on_person(person)
+      return unless person
+
+      person.cache_active_years
+      person.save(validate: false)
     end
 
   end
