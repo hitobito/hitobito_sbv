ddf645c5d5b199324715c893ec7832f47080b4fc
Andreas Maierhofer <a.maierhofer@gmail.com>
2019-04-26 16:06:34 +0200

    Include reported members in subgroup export
    refs hitobito/hitobito#728

--- app/domain/export/tabular/groups/address_list.rb
+++ app/domain/export/tabular/groups/address_list.rb
@@ -1,7 +1,7 @@
 module Export::Tabular::Groups
   class AddressList < List
     def attributes
-      %w(name type address zip_code town country)
+      %w(name type address zip_code town country reported_members)
     end
   end
 end
