76d53b7964fd25611c845fae249c736047d13b97
Matthias Viehweger <viehweger@puzzle.ch>
2019-06-03 16:39:11 +0200

    Conform to linter and styleguide

--- app/jobs/sbv/export/subgroups_export_job.rb
+++ app/jobs/sbv/export/subgroups_export_job.rb
@@ -1,3 +1,8 @@
+#  Copyright (c) 2019, Schweizer Blasmusikverband. This file is part of
+#  hitobito_sbv and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sbv.
+
 module Sbv::Export::SubgroupsExportJob
 
   def initialize(user_id, group, options)
@@ -8,14 +13,14 @@ module Sbv::Export::SubgroupsExportJob
   private
 
   def entries
-    super.where(type: types.collect(&:sti_name) )
+    super.where(type: types.collect(&:sti_name))
   end
 
   def types
     [
       Group::Mitgliederverband,
       Group::Regionalverband,
-      Group::Verein,
+      Group::Verein
     ]
   end
 
