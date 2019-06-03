0b452e5457ec9ff60f977293cebf357814f23f8e
Matthias Viehweger <viehweger@puzzle.ch>
2019-06-03 16:39:11 +0200

    Conform to linter and styleguide

--- app/domain/export/tabular/groups/address_list.rb
+++ app/domain/export/tabular/groups/address_list.rb
@@ -1,7 +1,13 @@
+#  Copyright (c) 2019, Schweizer Blasmusikverband. This file is part of
+#  hitobito_sbv and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sbv.
+
 module Export::Tabular::Groups
   class AddressList < List
     def attributes
-      %w(name type mitgliederverband contact address zip_code town country reported_members besetzung klasse unterhaltungsmusik)
+      %w(name type mitgliederverband contact address zip_code town country
+         reported_members besetzung klasse unterhaltungsmusik)
     end
   end
 end
