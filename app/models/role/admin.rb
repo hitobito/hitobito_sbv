ed07c4baea8db6104558e9b8f85b61c99fd89fbf
Matthias Viehweger <viehweger@puzzle.ch>
2019-06-04 13:37:57 +0200

    Remove duplicate License-header

--- app/models/role/admin.rb
+++ app/models/role/admin.rb
@@ -1,6 +1,4 @@
-# encoding: utf-8
-
-#  Copyright (c) 2012-2018, Schweizer Blasmusikverband. This file is part of
+#  Copyright (c) 2012-2019, Schweizer Blasmusikverband. This file is part of
 #  hitobito_sbv and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_sbv.
@@ -18,12 +16,6 @@
 #  updated_at :datetime
 #  deleted_at :datetime
 #
-
-
-#  Copyright (c) 2012-2018, Schweizer Blasmusikverband. This file is part of
-#  hitobito_sbv and licensed under the Affero General Public License version 3
-#  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito_sbv.
 
 class Role::Admin < Role
   self.permissions = [:layer_and_below_full, :admin, :impersonation, :finance, :song_census]
