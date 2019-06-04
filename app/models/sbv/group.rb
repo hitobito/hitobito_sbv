d99a6a801890ce5eb287c9461367c1b4b53c71dd
Matthias Viehweger <viehweger@puzzle.ch>
2019-06-04 09:23:39 +0200

    Conform to linter and styleguide

--- app/models/sbv/group.rb
+++ app/models/sbv/group.rb
@@ -1,6 +1,4 @@
-# encoding: utf-8
-
-#  Copyright (c) 2012-2018, Schweizer Blasmusikverband. This file is part of
+#  Copyright (c) 2012-2019, Schweizer Blasmusikverband. This file is part of
 #  hitobito_sbv and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_sbv.
@@ -8,7 +6,7 @@
 module Sbv::Group
   extend ActiveSupport::Concern
 
-  included do
+  included do # rubocop:disable Metrics/BlockLength
     root_types Group::Root
 
     include I18nSettable
@@ -51,7 +49,7 @@ module Sbv::Group
   end
 
   def mitgliederverband
-    self.ancestors.where(type: Group::Mitgliederverband).first
+    ancestors.find_by(type: Group::Mitgliederverband)
   end
 
   # actual other parents, secondary and tertiary
