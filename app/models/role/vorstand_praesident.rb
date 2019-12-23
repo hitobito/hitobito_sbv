34b245518d3af680bb800888f837d0849cc74152
Matthias Viehweger <viehweger@puzzle.ch>
2019-12-23 07:43:43 +0100

    Remove obsolete and duplicate comments

--- app/models/role/vorstand_praesident.rb
+++ app/models/role/vorstand_praesident.rb
@@ -17,12 +17,6 @@
 #  deleted_at :datetime
 #
 
-
-#  Copyright (c) 2012-2013, Puzzle ITC GmbH. This file is part of
-#  hitobito_generic and licensed under the Affero General Public License version 3
-#  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito_generic.
-
 class Role::VorstandPraesident < Role
   self.permissions = [:layer_full, :layer_and_below_read, :contact_data, :festival_participation]
 end
