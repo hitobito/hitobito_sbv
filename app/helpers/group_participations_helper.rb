428e0135e40bc2684f64f709eec35f7a34f5a8f9
Aleksandar Radunovic <a.radunovic@pm.me>
2019-12-30 14:03:57 +0100

    Add select option localization

--- app/helpers/group_participations_helper.rb
+++ app/helpers/group_participations_helper.rb
@@ -1,19 +1,28 @@
 module GroupParticipationsHelper
   def music_styles_selection
-    Event::GroupParticipation::MUSIC_CLASSIFICATIONS.map { |h| h[:style] }
+    Event::GroupParticipation::MUSIC_CLASSIFICATIONS.map { |h| music_i18n_option(:music_style, h[:style]) }
   end
 
   def music_types_selection_for(music_style)
-    music_types_for(music_style).keys
+    music_types_for(music_style).keys.map { |v| music_i18n_option(:music_type, v) }
   end
 
   def music_level_selections_for(music_style)
     music_types_for(music_style).map do |type, levels|
-      content_tag(:div, hidden: true, class: type) { options_for_select(levels || []) }
+      values = levels.map { |level| music_i18n_option(:music_level, level) }
+
+      content_tag(:div, hidden: true, class: type) { options_for_select(values || []) }
     end.join.html_safe
   end
 
   def music_types_for(music_style)
     Event::GroupParticipation::MUSIC_CLASSIFICATIONS.find { |h| h[:style] == music_style }[:types]
+  end
+
+  def music_i18n_option(kind, value)
+    [
+      t("activerecord.attributes.#{Event::GroupParticipation.name.underscore}.#{kind.to_s.pluralize}.#{value}"),
+      value
+    ]
   end
 end
