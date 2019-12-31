9d8574a293372c23cb63498d5e99058e7a3ac7b5
Aleksandar Radunovic <a.radunovic@pm.me>
2019-12-31 16:17:41 +0100

    Revert temporary comment

--- db/migrate/20191231135200_add_preferred_play_days_on_group_participations.rb
+++ db/migrate/20191231135200_add_preferred_play_days_on_group_participations.rb
@@ -1,6 +1,6 @@
 class AddPreferredPlayDaysOnGroupParticipations < ActiveRecord::Migration
   def change
-    # add_column :event_group_participations, :preferred_play_day_1, :integer
-    # add_column :event_group_participations, :preferred_play_day_2, :integer
+    add_column :event_group_participations, :preferred_play_day_1, :integer
+    add_column :event_group_participations, :preferred_play_day_2, :integer
   end
 end
