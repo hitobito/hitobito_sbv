1ad6615dbdbec59731a4c164d2b84ba75486aa26
Matthias Viehweger <viehweger@puzzle.ch>
2020-01-01 13:49:36 +0100

    Repair specs

--- spec/models/event/festival_spec.rb
+++ spec/models/event/festival_spec.rb
@@ -1,4 +1,4 @@
-#  Copyright (c) 2019, Schweizer Blasmusikverband. This file is part of
+#  Copyright (c) 2019-2020, Schweizer Blasmusikverband. This file is part of
 #  hitobito_sbv and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_sbv.
@@ -6,13 +6,48 @@
 require 'spec_helper'
 
 describe Event::Festival do
-  it 'knows which event are participatable' do
-    expect(described_class.participatable).to be_an Array
-    pending
+  let(:festival) { events(:festival) }
+  let(:group) { groups(:musikgesellschaft_aarberg) }
+
+  it 'knows which events are participatable' do
+    do_not_participate(group, festival)
+
+    list = described_class.participatable(group)
+
+    expect(list).to be_one
   end
 
-  it 'knows which groups have already applied' do
-    expect(described_class.participation_by(nil)).to be_an Array
-    pending
+  it 'does not count events as participatable if already applied' do
+    participate(group, festival)
+
+    list = described_class.participatable(group)
+
+    expect(list).to be_empty
+  end
+
+  it 'knows events a group has already applied to' do
+    participate(group, festival)
+
+    relation = described_class.participation_by(group)
+
+    expect(relation.to_a).to eql [festival]
+  end
+
+  private
+
+  def participate(group, event)
+    Event::GroupParticipation.create(
+      group_id: group.id,
+      event_id: festival.id
+    )
+  end
+
+  def do_not_participate(group, event)
+    count = Event::GroupParticipation.where(
+      group_id: group.id,
+      event_id: festival.id
+    ).count
+
+    expect(count).to be 0
   end
 end
