5b6bc7cb28317cd5d26789253a3fb5ac02ede2fa
Matthias Viehweger <viehweger@puzzle.ch>
2020-01-23 11:29:46 +0100

    Align spec-setup with usage in app

--- spec/jobs/export/subgroups_export_job_spec.rb
+++ spec/jobs/export/subgroups_export_job_spec.rb
@@ -7,7 +7,7 @@ require 'spec_helper'
 require 'csv'
 
 describe Export::SubgroupsExportJob do
-  let(:subject) { described_class.new(people(:admin), groups(:hauptgruppe_1), {}) }
+  let(:subject) { described_class.new(people(:admin), groups(:hauptgruppe_1).id, {}) }
 
   it 'only exports Verband and Verein group types' do
     names = subject.send(:entries).collect { |e| e.class.sti_name }.uniq
