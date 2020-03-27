1df26d6e66b9db016a045f12d0d9ec8a8be5a4a7
Matthias Viehweger <viehweger@puzzle.ch>
2020-03-27 01:05:13 +0100

    Allow to export the Lohnsummen

---
+++ app/controllers/lohnsummen_controller.rb
@@ -0,0 +1,26 @@
+#  Copyright (c) 2020, Schweizer Blasmusikverband. This file is part of
+#  hitobito_sbv and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sbv.
+
+class LohnsummenController < ApplicationController
+  skip_authorize_resource
+  before_action :authorize_class
+
+  def show
+    groups = group.descendants.where(type: 'Group::Verein')
+    csv = Export::Tabular::Groups::LohnsummenList.csv(groups)
+    send_data csv, type: :csv, disposition: 'attachment'
+  end
+
+  private
+
+  def authorize_class
+    authorize!(:show_uv_lohnsummen, group)
+  end
+
+  def group
+    @group ||= Group.find(params[:group_id])
+  end
+
+end
