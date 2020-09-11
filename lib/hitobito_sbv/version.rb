e379431aa71dab4eea38aec97b06da85fc7217cf
Matthias Viehweger <viehweger@puzzle.ch>
2020-09-11 15:06:38 +0200

    Bump Version for Release

--- lib/hitobito_sbv/version.rb
+++ lib/hitobito_sbv/version.rb
@@ -1,11 +1,10 @@
-# encoding: utf-8
 # frozen_string_literal: true
 
-#  Copyright (c) 2012-2018, Schweizer Blasmusikverband. This file is part of
+#  Copyright (c) 2012-2020, Schweizer Blasmusikverband. This file is part of
 #  hitobito_sbv and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_sbv.
 
 module HitobitoSbv
-  VERSION = '1.22'
+  VERSION = '1.22.4'
 end
