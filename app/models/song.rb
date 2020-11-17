b9340288697080fe0eb8202247cd9d91a430e5cb
Matthias Viehweger <viehweger@puzzle.ch>
2020-11-17 12:03:27 +0100

    Change whitespace, indentation or line-breaks

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,10 @@
-# encoding: utf-8
 # frozen_string_literal: true
 
 #  Copyright (c) 2012-2018, Schweizer Blasmusikverband. This file is part of
 #  hitobito_sbv and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_sbv.
+
 
 # == Schema Information
 #
@@ -25,8 +25,8 @@ class Song < ActiveRecord::Base
   scope :list, -> { order(:title) }
 
   validates_by_schema
-  validates :title,
-    uniqueness: { scope: [:composed_by, :arranged_by, :published_by], case_sensitive: true }
+  validates :title, uniqueness: { scope: [:composed_by, :arranged_by, :published_by],
+                                  case_sensitive: true }
 
   def to_s
     title
