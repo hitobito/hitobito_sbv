37746e51abbf2beb5bd4dd36dc4668ed7a300bb1
Carlo Beltrame <carlo@beltra.me>
2021-02-03 16:33:32 +0100

    Add missing fabricators for classes that should not be used directly

---
+++ spec/fabricators/historic_role_fabricator.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+# == Schema Information
+#
+# Table name: roles
+#
+#  id         :integer          not null, primary key
+#  person_id  :integer          not null
+#  group_id   :integer          not null
+#  type       :string(255)      not null
+#  label      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#  deleted_at :datetime
+#
+
+[ Role::MitgliederMitglied, Role::MitgliederPassivmitglied ].collect { |r| r.name.to_sym }.each do |t|
+  Fabricator(t, from: :role, class_name: t)
+end
