1cafdd251ebc37640f3d3e56c7d7042dc67405ed
Luca Brügger <hello@lucabruegger.ch>
2021-09-06 08:02:27 +0200

    Implement review

--- db/migrate/20210827111245_add_correspondence_language_to_person_by_layer.rb
+++ db/migrate/20210827111245_add_correspondence_language_to_person_by_layer.rb
@@ -7,17 +7,22 @@
 
 class AddCorrespondenceLanguageToPersonByLayer < ActiveRecord::Migration[6.0]
   def up
-    people_without_language.find_each do |person|
-      person.correspondence_language = person.primary_group.correspondence_language
-      person.save
+    people = people_without_language
+    say_with_time('updating people to have default language set') do
+      people.find_each do |person_without_language|
+        person_without_language.correspondence_language = person_without_language.primary_group.correspondence_language
+        person_without_language.save
+      end
     end
   end
 
   private
 
   def people_without_language
-    supported_languages = Settings.application.correspondence_languages.to_h.keys.map(&:to_s)
-    Person.where("people.correspondence_language NOT IN (?) OR people.correspondence_language IS NULL", supported_languages)
+    say_with_time('gathering people who do not have a language set') do
+      supported_languages = Settings.application.correspondence_languages.to_h.keys.map(&:to_s)
+      Person.where("people.correspondence_language NOT IN (?) OR people.correspondence_language IS NULL", supported_languages)
+    end
   end
 
 end
