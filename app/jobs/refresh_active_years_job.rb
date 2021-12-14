6c25d97d28c9ecac3c2123cf41188fe54c084667
Matthias Viehweger <viehweger@puzzle.ch>
2021-12-14 23:27:42 +0100

    BaseJob#perform handles both locale and rescheduling
    BaseJob#perform_internal is the method that should be overwritten

--- app/jobs/refresh_active_years_job.rb
+++ app/jobs/refresh_active_years_job.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 
-class RefreshActiveYearsJob < RecurringJob
+#  Copyright (c) 2019-2021, Schweizer Blasmusikverband. This file is part of
+#  hitobito_sbv and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sbv.
 
-  def perform
+class RefreshActiveYearsJob < RecurringJob
+  private
+
+  def perform_internal
     Person.find_each do |person|
       person.cache_active_years
       person.save(validate: false)
     end
   end
 
-  private
-
   def next_run
     Time.new(Time.zone.now.year.succ, 1, 1, 5, 0).in_time_zone
   end
-
 end
