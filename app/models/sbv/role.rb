943438d3482eec151034030f611fb14f79862343
Nils Rauch <rauch@puzzle.ch>
2022-05-10 08:36:17 +0200

    Remove wagon specific role timestamp fields, refs: https://github.com/hitobito/hitobito/issues/1714

--- app/models/sbv/role.rb
+++ app/models/sbv/role.rb
@@ -3,22 +3,6 @@
 module Sbv::Role
   extend ActiveSupport::Concern
 
-  included do
-    self.used_attributes += [:created_at]
-
-    validates :created_at, presence: true, if: :deleted_at
-
-    validates_date :created_at,
-                   if: :deleted_at,
-                   on_or_before: :deleted_at,
-                   on_or_before_message: :cannot_be_later_than_deleted_at
-
-    validates_date :created_at,
-                   allow_nil: true,
-                   on_or_before: -> { Time.zone.today },
-                   on_or_before_message: :cannot_be_later_than_today
-  end
-
   Role::Types::Permissions << :song_census
 
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -11,47 +11,6 @@ describe Role do
 
   let(:role) { roles(:member) }
 
-  it "validates presence of created_at if deleted_at is present" do
-    role.update(
-      created_at: nil,
-      deleted_at: Time.zone.now
-    )
-
-    expect(role).not_to be_valid
-    expect(role.errors.full_messages).
-      to include('Eintritt muss ausgefüllt werden')
-  end
-
-  it "validate created_at is earlier than today" do
-    role.update_attribute(:created_at, 1.day.from_now)
-
-    expect(role).not_to be_valid
-    expect(role.errors.full_messages).
-      to include('Eintritt kann nicht später als heute sein')
-  end
-
-  it "validate created_at can be equal with today" do
-    role.update_attribute(:created_at, Time.zone.today)
-
-    expect(role).to be_valid
-  end
-
-  it "validate created_at is earlier than deleted_at" do
-    role.update_attribute(:created_at, 1.day.ago)
-    role.update_attribute(:deleted_at, 3.day.ago)
-
-    expect(role).not_to be_valid
-    expect(role.errors.full_messages).
-      to include('Eintritt muss vor oder am selben Tag wie der Austritt sein')
-  end
-
-  it "validate created_at can be equal with deleted_at" do
-    role.update_attribute(:created_at, Time.zone.now)
-    role.update_attribute(:deleted_at, Time.zone.now)
-
-    expect(role).to be_valid
-  end
-
   it 'triggers a recalculation of the persons active_years' do
     person = role.person
     person.update_active_years # cache active_years
