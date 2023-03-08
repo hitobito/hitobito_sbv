e0699ca2c9952d5cc3a7f0b904bbe4f1dfa69feb
Carlo Beltrame <carlo@beltra.me>
2023-03-08 17:05:50 +0100

    Fix moving groups in specific conditions
    Fixes hitobito/hitobito#736
    Fixes https://help.puzzle.ch/#ticket/zoom/5707

--- app/models/sbv/group/nested_set.rb
+++ app/models/sbv/group/nested_set.rb
@@ -1,29 +1,46 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2019, Schweizer Blasmusikverband. This file is part of
+#  Copyright (c) 2019-2023, Schweizer Blasmusikverband. This file is part of
 #  hitobito_sbv and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_sbv.
 
 module Sbv
   module Group::NestedSet
-    private
+    extend ActiveSupport::Concern
 
-    # rubocop:disable Style/IfInsideElse,Style/RedundantSelf,Rails/FindEach
-    # copied from awesome_nested_set to accomodate for a little change
-    def find_left_neighbor(parent, order_attribute, ascending)
-      left = nil
-
-      # change from upsteam version to omit secondary and tertiary children
-      parent.children.where(parent_id: parent.id).each do |n|
-        if ascending
-          left = n if n.send(order_attribute) < self.send(order_attribute)
-        else
-          left = n if n.send(order_attribute) > self.send(order_attribute)
-        end
+    included do
+      def direct_children_of(group)
+        group.children.where(parent_id: group.id)
       end
-      left
+
+      # rubocop:disable Style/IfInsideElse,Style/RedundantSelf,Rails/FindEach
+      # copied from awesome_nested_set to accomodate for a little change
+      def find_left_neighbor(parent, order_attribute, ascending)
+        left = nil
+
+        # change from upsteam version to omit secondary and tertiary children
+        direct_children_of(parent).each do |n|
+          if ascending
+            left = n if n.send(order_attribute) < self.send(order_attribute)
+          else
+            left = n if n.send(order_attribute) > self.send(order_attribute)
+          end
+        end
+        left
+      end
+      # rubocop:enable Style/IfInsideElse,Style/RedundantSelf,Rails/FindEach
     end
-    # rubocop:enable Style/IfInsideElse,Style/RedundantSelf,Rails/FindEach
+
+    def move_required?
+      (@move_to_new_parent_id != false || @move_to_new_name != false) &&
+        parent_id &&
+        direct_children_of(parent).count > 1
+    end
+
+    def move_to_most_left_if_change
+      first = direct_children_of(parent).first
+      move_to_left_of(first) unless first == self
+    end
   end
 end
