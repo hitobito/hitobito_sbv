3aa18b58ba7fa9bc1f896cef055bf91d0f2b68f0
Nils Rauch <nils.rauch@gmail.com>
2023-03-17 11:30:13 +0100

    Fix finding vereins kassier or praesident

--- app/domain/invoice_lists/verein_membership_fee_recipient_finder.rb
+++ app/domain/invoice_lists/verein_membership_fee_recipient_finder.rb
@@ -3,8 +3,10 @@ class InvoiceLists::VereinMembershipFeeRecipientFinder
   def self.find_recipient(verein_id)
     # TODO Add Rechnungskontakt
 
-    recipient_role = Group::VereinVorstand::Kassier.find_by(group_id: verein_id) ||
-      Group::VereinVorstand::Praesident.find_by(group_id: verein_id) ||
+    vorstand_id = Group::VereinVorstand.where(parent_id: verein_id).first.id
+
+    recipient_role = Group::VereinVorstand::Kassier.find_by(group_id: vorstand_id) ||
+      Group::VereinVorstand::Praesident.find_by(group_id: vorstand_id) ||
       Group::Verein::Admin.find_by(group_id: verein_id)
 
     recipient_role.person_id
