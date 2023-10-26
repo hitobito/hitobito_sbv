fe904d3046c948f8b7128c43d2c3556e04ccc865
Daniel Illi <illi@puzzle.ch>
2023-10-26 13:42:25 +0200

    Fix active_year_performance_spec

--- spec/performance/active_year_performance_spec.rb
+++ spec/performance/active_year_performance_spec.rb
@@ -17,10 +17,12 @@ describe 'VeteranYears', performance: true do
   let(:person) { people(:member) }
 
   before :each do
+    person.roles.each {|role| role.really_destroy! }
     Role.create!(person: person, group: group, created_at: 20.years.ago, deleted_at: 17.years.ago, type: 'Group::VereinMitglieder::Mitglied')
     Role.create!(person: person, group: group, created_at: 15.years.ago, deleted_at: 13.years.ago, type: 'Group::VereinMitglieder::Mitglied')
     Role.create!(person: person, group: group, created_at: 10.years.ago, deleted_at:  7.years.ago, type: 'Group::VereinMitglieder::Mitglied')
     Role.create!(person: person, group: group, created_at:  5.years.ago, deleted_at:  3.years.ago, type: 'Group::VereinMitglieder::Mitglied')
+    Role.create!(person: person, group: group, created_at:  1.year.ago, deleted_at: nil, type: 'Group::VereinMitglieder::Mitglied')
   end
 
   def measure(max_time, &block)
@@ -33,13 +35,13 @@ describe 'VeteranYears', performance: true do
 
   it 'load active years' do
     measure(FETCH_YEARS_TIME) do
-      expect(person.active_years).to be == 10
+      expect(person.active_years).to be == 15
     end
   end
 
   it 'load prognostic active years' do
     measure(FETCH_YEARS_TIME) do
-      expect(person.prognostic_active_years).to be == 11
+      expect(person.prognostic_active_years).to be == 16
     end
   end
 
