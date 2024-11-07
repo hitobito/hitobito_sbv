31fd145ca1c5a2f0587f882a43853e52e601e6d1
megli2 <egli@puzzle.ch>
2024-11-07 08:26:42 +0100

    Use config call to let initializer be autoladed again

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,4 +1,6 @@
 if Delayed::Job.table_exists? && !Rails.env.test?
   # schedule cron jobs here
-  RefreshActiveYearsJob.new.schedule
+   Rails.application.config.to_prepare do
+     RefreshActiveYearsJob.new.schedule
+  end
 end
