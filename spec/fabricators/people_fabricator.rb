026a5807ec88a8a81e6aa0cb6921efd39bf80531
Niklas <68543592+njaeggi@users.noreply.github.com>
2026-01-16 18:58:16 +0100

    Use language instead of correspondence_language (hitobito/hitobito#3872) (#174)

--- spec/fabricators/people_fabricator.rb
+++ spec/fabricators/people_fabricator.rb
@@ -9,5 +9,5 @@ Fabrication.manager.schematics[:person_without_birthday] =
 
 Fabricator(:person, from: :person_without_birthday) do
   birthday { Faker::Date.birthday }
-  correspondence_language { "de" }
+  language { "de" }
 end
